f87eb5ff7db660444d9e0672bb459b6776911384
Hüseyin Ünalan <huseyin66unalan@gmail.com>
2023-02-28 11:17:01 +0300

    Update 4

--- resources/views/admin/about/edit_about.blade.php
+++ resources/views/admin/about/edit_about.blade.php
@@ -9,7 +9,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0">Update About</h4>
+                        <h4 class="mb-sm-0">Update Abouta</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
